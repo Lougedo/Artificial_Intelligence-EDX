984c011fdd54f1320e707e4fb9fd094daae5150f
Lougedo <javier.lougedo.lorente@gmail.com>
2020-07-26 11:45:04 +0200

    Shoppig ended

--- 5_Learning/shopping/shopping.py
+++ 5_Learning/shopping/shopping.py
@@ -60,7 +60,6 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    data = list()
     evidence = list()
     labels = list()
     with open(filename) as f:
